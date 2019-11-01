50b16ecaba59bb41c86a167ea37d5be77b8317ad
Andrey Molotkov <molotkov.and@gmail.com>
2019-11-02 01:35:42 +0300

    Added Makefile to build google tests

---
+++ test/ut/main.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int factorial(int n);
+
+TEST(FactorialTest, HandlesZeroInput) {
+    ASSERT_EQ(factorial(0), 1);
+}
+
+TEST(FactorialTest, HandlesPositiveInput) {
+    ASSERT_EQ(factorial(1), 1);
+    ASSERT_EQ(factorial(2), 2);
+    ASSERT_EQ(factorial(3), 6);
+    ASSERT_EQ(factorial(8), 40320);
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello!" << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    int result = 1;
+    while (n > 1) {
+        result *= n--;
+    }
+    return result;
+}
